4d4595c0434ff7dfbebf3320cce7245956311bd1
Daoud Clarke <daoud.clarke@gmail.com>
2014-01-29 09:24:21 +0000

    Implement in terms of possible substitutions

--- code/probsem/learn.py
+++ code/probsem/learn.py
@@ -3,6 +3,8 @@
 # Learn distributions for probabilistic semantics using expectation maximisation
 
 from numpy.random import rand
+from itertools import product
+from copy import deepcopy
 
 det = 'det'
 noun = 'noun'
@@ -18,7 +20,8 @@ class Learner(object):
     def __init__(self):
         self.dims = {x: 2 for x in words + functions}
         self.hidden_dims = 2
-        self.theta = {}
+        self.words = {}
+        self.functions = {}
 
     def initialise(self, theory):
         for sentence in theory:
@@ -30,11 +33,11 @@ class Learner(object):
     def initialise_sub(self, f):
         if f[0] == 'w':
             assert f[1] in words
-            self.theta[tuple(f[1:])] = rand(self.dims[f[1]], self.hidden_dims)
+            self.words[tuple(f[1:])] = rand(self.dims[f[1]], self.hidden_dims)
         elif f[0] == 'f':
             assert f[1] in functions
             f2, f3 = f[2][1], f[3][1]
-            self.theta[(f[1], f2, f3)] = rand(self.dims[f[1]], self.dims[f2], self.dims[f3], self.hidden_dims)
+            self.functions[(f[1], f2, f3)] = rand(self.dims[f[1]], self.dims[f2], self.dims[f3], self.hidden_dims)
             self.initialise_sub(f[2])
             self.initialise_sub(f[3])
         else:
@@ -42,13 +45,62 @@ class Learner(object):
 
     def prob(self, theory):
         for h in range(self.hidden_dims):
+            # word_indexes = [range(self.dims[x[0]]) for x in self.words.keys()]
+            # function_indexes = [range(self.dims[x[0]]) for x in self.functions.keys()]
+            # for x in product(*(word_indexes + function_indexes)):
+            #     for values, index in zip(self.words.items(), x[:len(self.words)]):
+            #         print index, values[1][h, index]
+
+            #     for values, index in zip(self.functions.items(), x[len(self.words):]):
+            #         print index, values[1][h, index]
+            
             cached = {}
+        
+            prob = 1.0
             for sentence in theory:
                 truth, f, value = sentence
                 if truth != 'truth':
                     raise ValueError('Expected truth assertion')
-                self.prob_sub(f, h, cached)
+                sentence_prob = self.prob_sub(f, value, cached)
+                prob *= sentence_prob
+            return prob
         
+    def substitute_values(self, theory):
+        """
+        Returns an iterable of all possible value assignments for a theory
+        """
+        
+    def substitute_expression_values(self, expression, subs):
+        if expression[0] == 'w':
+            key = (expression[1], expression[2])
+            expression_copy = deepcopy(expression)
+            expression_copy.append(None)
+            if key in subs:
+                expression_copy[-1] = subs[key]
+                yield expression_copy, subs
+            else:
+                for i in range(self.dims[expression[1]]):
+                    subs_copy = deepcopy(subs)
+                    subs_copy[key] = i
+                    expression_copy[-1] = i
+                    yield expression_copy, subs_copy
+        else:
+            for new_expression1, new_subs1 in self.substitute_expression_values(expression[2], subs):
+                for new_expression2, new_subs2 in self.substitute_expression_values(expression[3], new_subs1):
+                    key = (expression[1], new_expression1[-1], new_expression2[-1])
+                    expression_copy = deepcopy(expression)
+                    expression_copy.append(None)
+                    expression_copy[2] = new_expression1
+                    expression_copy[3] = new_expression2
+                    if key in new_subs2:
+                        expression_copy[-1] = new_subs2[key]
+                        yield expression_copy, new_subs2
+                    else:
+                        for i in range(self.dims[expression[1]]):
+                            subs_copy = deepcopy(new_subs2)
+                            subs_copy[key] = i
+                            expression_copy[-1] = i
+                            yield expression_copy, subs_copy
+                        
+                
 
-    def prob_sub(self, f, h):
-        
--- code/probsem/learn_test.py
+++ code/probsem/learn_test.py
@@ -1,7 +1,8 @@
 from probsem.learn import Learner, words, functions
-from pytest import fixture
+import pytest
 
-@fixture
+
+@pytest.fixture
 def sentence():
     s = 's'
     det = 'det'
@@ -16,30 +17,73 @@ def sentence():
     return f(s, f(np, w(det, 'some'), w(noun, 'cats')),
              f(vp, w(verb, 'like'), f(np, w(det, 'all'), w(noun, 'dogs'))))
 
+@pytest.fixture
+def learner():
+    s = sentence()
+    theory = [truth(s, True)]
+    l = Learner()
+    l.initialise(theory)
+    return l
+
 def truth(l, v):
     return ['truth', l, v]
 
-def test_learn(sentence):
+def test_initialise(sentence):
     data = [truth(sentence, True)]
     print data
 
     learner = Learner()
     learner.initialise(data)
-    print learner.theta
+    print "Words: ", learner.words
+    print "Functions: ", learner.functions
 
-    assert len(learner.theta[('det', 'some')]) > 0
+    assert len(learner.words[('det', 'some')]) > 0
+    assert len(learner.functions[('s', 'np', 'vp')]) > 0
 
 
-def test_consistency(sentence):
+@pytest.mark.xfail
+def test_consistency(sentence, learner):
     data1 = [truth(sentence, True)]
     data2 = [truth(sentence, True), truth(sentence, True)]
     
-    learner = Learner()
-    learner.initialise(data1)
-
     prob1 = learner.prob(data1)
     prob2 = learner.prob(data2)
+
+    print data1
 
     assert prob1 >= 0.0
     assert prob1 <= 1.0
     assert prob1 == prob2
+
+@pytest.mark.xfail
+def test_substitute_values(sentence, learner):
+    data = [truth(sentence, True), truth(sentence, True)]
+    
+    learner = Learner()
+    for s in learner.substitute_values(data1):
+        print s
+    
+def test_substitute_expression_word(learner):
+    expression = ['w', 'det', 'some']
+    values = list(learner.substitute_expression_values(expression, {}))
+
+    assert len(values) == 2
+    for e, subs in values:
+        print e, subs
+        assert len(subs) == 1
+        assert e[:-1] == expression
+        assert e[-1] in [0,1]
+        
+def test_substitute_expression_recursion(learner):
+    expression = ['f', 'np', ['w', 'det', 'some'], ['w', 'noun', 'cats']]
+    values = list(learner.substitute_expression_values(expression, {}))
+    assert len(values) == 8
+    for e, subs in values:
+        print e, subs
+        assert len(subs) == 3
+        assert e[:2] == ['f', 'np']
+        assert e[-1] in [0,1]
+        assert e[2][-1] in [0,1]
+        assert e[3][-1] in [0,1]
+
+    
